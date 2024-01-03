d56e2b1994dd54370f2b3fcafb45b592b1932343
samolego <34912839+samolego@users.noreply.github.com>
2024-01-03 10:36:54 +0100

    Non-generic sprites
    Doesn't make sense as they are hardcoded

--- sources_1/new/index2sprite.template.vhd
+++ sources_1/new/index2sprite.template.vhd
@@ -3,12 +3,9 @@ use IEEE.STD_LOGIC_1164.all;
 use IEEE.NUMERIC_STD.all;
 
 entity index2sprite is
-    generic (
-        sprite_vector_width : integer -- length of sprite matrix reshaped into vector
-    );
     port (
         sprite_index : in std_logic_vector (4 downto 0);
-        sprite_image_bits : out std_logic_vector (sprite_vector_width - 1 downto 0)
+        sprite_image_bits : out std_logic_vector (255 downto 0)
     );
 end entity;
 architecture Behavioral of index2sprite is
